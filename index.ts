8de4dd3be4d31f1db87455424394af927d898d47
Rodrigo Martins de Azevedo <rooxalot@gmail.com>
2024-06-20 17:51:12 -0300

    Simple implementation of model invoke with streams

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+import 'dotenv/config';
+
+import { ChatOpenAI } from '@langchain/openai';
+
+const start = async () => {
+    const model = new ChatOpenAI(); // OpenAI Key is on env
+    const response = await model.stream('Which OpenAI version am I using right now?');
+
+    for await (const chunk of response) {
+        process.stdout.write(String(chunk?.content))
+    }
+};
+
+start();
