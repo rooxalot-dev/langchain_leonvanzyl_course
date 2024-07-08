b09a9ca7bfd465908f4d3773daa257d99774c80e
Rodrigo Martins de Azevedo <rooxalot@gmail.com>
2024-07-08 17:11:04 -0300

    Convert invoke method to chain format

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 import { ChatOpenAI } from '@langchain/openai';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
 
 const start = async () => {
     // OpenAI Key is on env
@@ -9,11 +10,17 @@ const start = async () => {
         temperature: 0.7,
         maxTokens: 1000,
         verbose: false // debug
-
     });
-    const aiResponse = await model.invoke('Which OpenAI version am I using right now?');
-    const usedTokens = aiResponse.usage_metadata?.total_tokens;
-    const content = aiResponse.content;
+
+    const templatePrompt = ChatPromptTemplate.fromTemplate(`
+        You are a comedian. Tell me a joke based on the following word: {inputWord}
+    `);
+
+    const chain = templatePrompt.pipe(model);
+    const aiChainResponse = await chain.invoke({ inputWord: 'Amazon' });
+    const usedTokens = aiChainResponse.usage_metadata?.total_tokens;
+    const content = aiChainResponse.content;
+
     console.log(`Used Tokens: ${usedTokens} - Content: ${content}`);
 };
 
