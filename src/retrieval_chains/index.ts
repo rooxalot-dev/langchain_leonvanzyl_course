4d3fd8d5a65ba3bb8afe647e2ca9ae918415ff3e
Rodrigo Martins de Azevedo <rooxalot@gmail.com>
2024-07-14 16:01:45 -0300

    Create memory vector store to insert documents and enable their retrieval based on the searched context, using semantic search

--- src/retrieval_chains/index.ts
+++ src/retrieval_chains/index.ts
@@ -1,8 +1,12 @@
-import { ChatOpenAI } from "@langchain/openai";
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
 import { StringOutputParser } from "@langchain/core/output_parsers";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { createRetrievalChain } from "langchain/chains/retrieval";
+
 
 export async function callChatWithDocumentContext(model: ChatOpenAI) {
     const prompt = ChatPromptTemplate.fromTemplate(`
@@ -13,12 +17,24 @@ export async function callChatWithDocumentContext(model: ChatOpenAI) {
 
     const loader = new CheerioWebBaseLoader('https://js.langchain.com/v0.2/docs/concepts/#langchain-expression-language')
     const documents = await loader.load();
+    const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 200, chunkOverlap: 20 });
+    const splitDocuments = await textSplitter.splitDocuments(documents);
+    const vectorStore = await MemoryVectorStore.fromDocuments(
+        splitDocuments,
+        new OpenAIEmbeddings()
+    );
 
-    const chain = await createStuffDocumentsChain({
+    const combineDocumentsChain = await createStuffDocumentsChain({
         llm: model,
         prompt,
         outputParser: new StringOutputParser(),
     });
+
+    const chain = await createRetrievalChain({
+        retriever: vectorStore.asRetriever(),
+        combineDocsChain: combineDocumentsChain
+    });
+
 
     const response = await chain.invoke({
         input: `What's the LCEL?`,
