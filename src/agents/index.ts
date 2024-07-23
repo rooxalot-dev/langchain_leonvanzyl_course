2b9da690b30cd655db80a4c2dcd8576c56a32895
Rodrigo Martins de Azevedo <rooxalot@gmail.com>
2024-07-22 22:40:48 -0300

    Create chatbot interation with readline lib, to get user input from the terminal

--- src/agents/index.ts
+++ src/agents/index.ts
@@ -1,3 +1,4 @@
+import readline from 'readline';
 import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
 import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts";
 import { createOpenAIFunctionsAgent, AgentExecutor } from "langchain/agents";
@@ -8,8 +9,8 @@ import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
 export async function callAgentWithTools(model: ChatOpenAI, tavilyApiKey: string) {
     const prompt = ChatPromptTemplate.fromMessages([
         ['system', `You're a helpful assistant call Max.`],
-        new MessagesPlaceholder('agent_scratchpad'), // This placeholer needs to be inserted, since it'll be used internally and automatically by the agent and tools
         ['human', '{input}'],
+        new MessagesPlaceholder('agent_scratchpad'), // This placeholer needs to be inserted, since it'll be used internally and automatically by the agent and tools
     ]);
 
     // Create and assign tools
@@ -27,9 +28,21 @@ export async function callAgentWithTools(model: ChatOpenAI, tavilyApiKey: string
 
     // Create and execute agent
     const agentExecutor = new AgentExecutor({ agent, tools });
-    const response = await agentExecutor.invoke({ input: `
-        Consider that the current year is ${new Date().getFullYear()}, answer the following question: 'What are the latest news for Alan Wake 2?'
-    ` });
 
-    return response;
+    const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+    
+
+    const askQuestion = () => {
+        rl.question('User: ', async (question) => {
+            if (question.toLowerCase() === 'exit') rl.close();
+
+            const response = await agentExecutor.invoke({ input: question });
+            console.log('Agent: ', response.output);
+
+            askQuestion();
+        });
+    };
+
+    askQuestion();
 }
